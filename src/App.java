aba0a88d762b4bc62823904b955dda4c53b5d89e
wendeehsu <b05705046@ntu.edu.tw>
2023-02-26 23:06:37 -0600

    feat: add read input

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         int a = 123;
         System.out.println(a*4);
+        System.out.println(Test.constant);
         Test.greet();
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class Test {
+    final static String constant = "aaa";
+
     public static void main(String[] args) {
+        // final String constant = "aaa";
+        String a = "a";
+        String b = a;
+        b = "b";
+        Scanner i = new Scanner(System.in);
+        System.out.println(a + " " + b + i.next());
         greet();
     }
 
     public static void greet() {
         System.out.println("update greet");
+        System.out.println(constant);
     }
 }
