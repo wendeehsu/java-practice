ee3ee21bcda3d55b3f97455a39d8d66f97b5159d
wendeehsu <b05705046@ntu.edu.tw>
2023-02-27 21:31:15 -0600

    feat: add nested loop

--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,15 @@ public class Test {
 
     public static void main(String[] args) {
         // testSwitchCase(15);
-        calculator();
+        // calculator();
+
+        for (int i = 1; i <= 10; i++) {
+            String s = "";
+            for (int j = 1; j <= 10; j++) {
+                s += i*j + " ";
+            }
+            System.out.println(s);
+        }
     }
 
     public static void greet() {
