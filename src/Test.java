54b4e14b7dc767c6dcc4e8aade8bace6b3247ab6
wendeehsu <b05705046@ntu.edu.tw>
2023-02-27 20:35:47 -0600

    feat: add calculator

--- src/Test.java
+++ src/Test.java
@@ -4,17 +4,50 @@ public class Test {
     final static String constant = "aaa";
 
     public static void main(String[] args) {
-        // final String constant = "aaa";
-        String a = "a";
-        String b = a;
-        b = "b";
-        Scanner i = new Scanner(System.in);
-        System.out.println(a + " " + b + i.next());
-        greet();
+        // testSwitchCase(15);
+        calculator();
     }
 
     public static void greet() {
         System.out.println("update greet");
         System.out.println(constant);
+    }
+
+    public static void testSwitchCase(int n) {
+        switch(n) {
+            case 10:
+                System.out.println("= 10");
+            case 15:
+                System.out.println("= 15");
+            case 20:
+                System.out.println("= 20");
+            default:
+                System.out.println("none");
+        }
+    }
+
+    public static void calculator() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("type the math: ");
+        int num1 = input.nextInt();
+        String ope = input.next();
+        int num2 = input.nextInt();
+
+        switch (ope) {
+            case "+":
+                System.out.println(num1 + num2);
+                break;
+            case "-":
+                System.out.println(num1 - num2);
+                break;
+            case "*":
+                System.out.println(num1 * num2);
+                break;
+            case "/":
+                System.out.println((float) num1 / num2);
+                break;
+            default:
+                System.out.println("non valid operator");
+        }
     }
 }
